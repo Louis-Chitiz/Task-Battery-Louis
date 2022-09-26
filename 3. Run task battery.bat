2c049e9a6c009dc86fe772551ebece1cf5507cfa
Ian-Goodall-Halliwell <90027009+Ian-Goodall-Halliwell@users.noreply.github.com>
2022-09-26 12:43:13 -0400

    Sync from PC6

--- 3. Run task battery.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-REM setlocal enabledelayedexpansion
-if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
-md Internal_Logs
-(
-call conda activate psytask
-python Tasks/mainscript.py
-)> "Internal_Logs/log_file_%RANDOM%.txt"
