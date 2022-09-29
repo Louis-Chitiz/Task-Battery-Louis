0db4347faf92de75f9615d36d9572edba442161f
Ian-Goodall-Halliwell <90027009+Ian-Goodall-Halliwell@users.noreply.github.com>
2022-09-29 14:49:02 -0400

    Sync from PC3

--- sync.bat
+++ sync.bat
@@ -2,11 +2,7 @@ cd "C:\Users\Smallwood Lab\Documents\GitHub\Task-Battery"
 
 git add --all
 
-<<<<<<< HEAD
 git commit -m "Sync from PC3"
-=======
-git commit -m "Sync from PC2"
->>>>>>> 2627950a8de37945913eea93d90989a660d4bf97
 
 git push
 
@@ -14,11 +10,7 @@ git pull
 
 git add --all
 
-<<<<<<< HEAD
 git commit -m "Sync from PC3"
-=======
-git commit -m "Sync from PC2"
->>>>>>> 2627950a8de37945913eea93d90989a660d4bf97
 
 git push
 
@@ -26,11 +18,7 @@ git pull
 
 git add --all
 
-<<<<<<< HEAD
 git commit -m "Sync from PC3"
-=======
-git commit -m "Sync from PC2"
->>>>>>> 2627950a8de37945913eea93d90989a660d4bf97
 
 git push
 
@@ -38,11 +26,7 @@ git pull
 
 git add --all
 
-<<<<<<< HEAD
 git commit -m "Sync from PC3"
-=======
-git commit -m "Sync from PC2"
->>>>>>> 2627950a8de37945913eea93d90989a660d4bf97
 
 git push
 
